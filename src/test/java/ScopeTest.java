05e5ca21c2f04b24ae0b0831f59508315d98d1c2
boatrain <yinger521999@gmail.com>
2020-09-28 11:17:28 +0800

    懒加载

--- src/test/java/ScopeTest.java
+++ src/test/java/ScopeTest.java
@@ -6,6 +6,7 @@ public class ScopeTest {
 
     @Test
     public void test() {
+        //多实例情况下，在getBean时才会new实例，而单实例情况下，默认在容器创建时就会new实例(除非指定@Lazy注解)
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ScopeConfig.class);
         Object bean = context.getBean("person");
         Object bean1 = context.getBean("person");
