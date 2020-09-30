c6a51827a8a4f36115322aff1b2c8267eac18e3b
boatrain <yinger521999@gmail.com>
2020-09-30 09:54:43 +0800

    AOPmaven导入

---
+++ src/main/java/config/AopConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+//为了说明aop原理
+@Configuration
+@EnableAspectJAutoProxy
+public class AopConfig {
+
+}
